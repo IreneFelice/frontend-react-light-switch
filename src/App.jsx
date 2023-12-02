15a4922d066954257836bf1940b77c80ba3d05ce
IreneFelice <i.f.buijing@gmail.com>
2023-12-02 10:03:53 +0100

    mijn uitwerking

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,28 @@ import React from 'react';
 import './App.css';
 
 function App() {
+    const [lightThing, setLight]= React.useState(false);
+    if (!lightThing){
+        console.log("uit")
+    } else {console.log("aan")}
+
+    if (lightThing){
     return (
+            <main className="on">
+                <section>
+                <div className="dot"></div>
+                <button type="button" onClick={() => setLight(false)}>Turn off</button>
+                </section>
+        </main>)}
+    else { return (
         <main className="off">
             <section>
                 <div className="dot"></div>
-                <button type="button">Turn on/off</button>
+                <button type="button" onClick={() => setLight(true)}>Turn on</button>
             </section>
-        </main>
-    );
-}
+        </main> ) }
+    }
+
+
 
 export default App;
